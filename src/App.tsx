bbabd6b7f61130bed0ebf727ada4258af3d85ee8
alzhan.nurtaza <alzhan.nurtaza@gmail.com>
2023-06-23 16:48:02 +0600

    blogposts and categories deleted

--- src/App.tsx
+++ src/App.tsx
@@ -24,18 +24,6 @@ import { AppIcon } from "./components/app-icon";
 import { Header } from "./components/header";
 import { ColorModeContextProvider } from "./contexts/color-mode";
 import {
-  BlogPostCreate,
-  BlogPostEdit,
-  BlogPostList,
-  BlogPostShow,
-} from "./pages/blog-posts";
-import {
-  CategoryCreate,
-  CategoryEdit,
-  CategoryList,
-  CategoryShow,
-} from "./pages/categories";
-import {
   DepartmentList,
   DepartmentShow,
   DepartmentEdit,
@@ -189,26 +177,6 @@ function App() {
                   list: () => null,
                   icon:<CalendarMonthOutlinedIcon/>,
                 },
-                {
-                  name: "blog_posts",
-                  list: "/blog-posts",
-                  create: "/blog-posts/create",
-                  edit: "/blog-posts/edit/:id",
-                  show: "/blog-posts/show/:id",
-                  meta: {
-                    canDelete: true,
-                  },
-                },
-                {
-                  name: "categories",
-                  list: "/categories",
-                  create: "/categories/create",
-                  edit: "/categories/edit/:id",
-                  show: "/categories/show/:id",
-                  meta: {
-                    canDelete: true,
-                  },
-                },
               ]}
               options={{
                 syncWithLocation: true,
@@ -282,18 +250,6 @@ function App() {
                       path="homePage"
                       element={<HomePage />}
                   />
-                  <Route path="/blog-posts">
-                    <Route index element={<BlogPostList />} />
-                    <Route path="create" element={<BlogPostCreate />} />
-                    <Route path="edit/:id" element={<BlogPostEdit />} />
-                    <Route path="show/:id" element={<BlogPostShow />} />
-                  </Route>
-                  <Route path="/categories">
-                    <Route index element={<CategoryList />} />
-                    <Route path="create" element={<CategoryCreate />} />
-                    <Route path="edit/:id" element={<CategoryEdit />} />
-                    <Route path="show/:id" element={<CategoryShow />} />
-                  </Route>
                   <Route path="*" element={<ErrorComponent />} />
                 </Route>
                 <Route
